f68721f1c2616808bdea24cadb9435c4de3e485a
Yoon-Orn Chin <57472778+ydchin@users.noreply.github.com>
2020-11-25 17:26:59 -0800

    fixes + color

--- options.js
+++ options.js
@@ -6,10 +6,17 @@ function rollDie(sides) {
 
 function timer(min) {
     let count;
-    function addSeconds() {
+
+    let startTime = Date.now();
+
+    let interval = setInterval(intervalTimer, 1000);
+
+    function intervalTimer() {
+        let dateVar = new Date();
+        let time = dateVar.getTime();
+        console.log(Math.floor((time - startTime) / 1000));
         console.log('time:', Math.floor(count / 60) + ':' + count % 60);
     }
-            
 }
 
 colorButtons.addEventListener('click', function (event) {
