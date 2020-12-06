d0a3469638a33af982366de4418385c5e1579e33
MarcusWheeler <wheemarc@oregonstate.edu>
2020-12-06 15:17:46 -0800

    This is handlebars implemented - CSS may be messier now, but I think We can delete the html if we want to at this point

--- server.js
+++ server.js
@@ -8,25 +8,29 @@
 
 var path = require('path');
 var express = require('express');
+var expressHandlebars = require('express-handlebars');
 
 var app = express();
 var port = process.env.PORT || 8000;
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'Main'}));
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res, next){
   console.log("==Hey I made it here");
-  res.status(200).sendFile(__dirname +'/public/options.html');
+res.status(200).render('gamePal', {optionsPage: true});
 });
 
 /*The next one is going to be the game with our choices. 
 Maybe I can parameterize the url? This will take some finagling*/
 app.get('/gameBuddy', function(req, res, next){
-  res.status(200).sendFile(__dirname+ '/public/ingame.html');
+  res.status(200).render('gamePal', {optionsPage: false, optionsPage: false});
 });
 
 app.get('*', function(req, res){
-  res.status(404).sendFile(__dirname + '/public/404.html');
+  res.status(404).render('404');
 });
 
 app.listen(port, function(){
