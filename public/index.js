7cecc8d0637e2b60523411076b590a8547bd4355
Yoon-Orn Chin <57472778+ydchin@users.noreply.github.com>
2020-12-07 01:34:44 -0800

    added js for confirm button + centered options

--- public/index.js
+++ public/index.js
@@ -1,8 +1,17 @@
 let colorButtons = document.querySelector('div.color-option-buttons');
 
+// Location of Confirm Button
+let confirmButton = document.querySelector('button.accept-button');
+
 let timerInterval;
 let timing;
 
+// Variables to store
+let numPlayers;
+let toggleTimer;
+let ptCounter;
+let gameDice;
+let styleColor;
 
 // used to test functions on click of any button
 colorButtons.addEventListener('click', function (event) {
@@ -16,4 +25,41 @@ colorButtons.addEventListener('click', function (event) {
     }
     else
         timer(1);
+});
+
+/*
+* Confirm Button
+* Store Data Here + Re-direct to new Game Pal page
+*/
+confirmButton.addEventListener('click', function (event) {
+
+	// Store Number of players
+	numPlayers = document.getElementById("players").value;
+
+	// Check if timer on or off
+	if(document.getElementById("timer-yes").checked) {
+		toggleTimer = document.getElementById("timer-yes").value;
+	}
+	else if(document.getElementById("timer-no").checked) {
+		toggleTimer = document.getElementById("timer-no").value;
+	}
+
+	// Check if point counter on or off
+	if(document.getElementById("points-yes").checked) {
+		ptCounter = document.getElementById("points-yes").value;
+	}
+	else if(document.getElementById("points-no").checked) {
+		ptCounter = document.getElementById("points-no").value;
+	}
+
+	// Check if dice counter on or off
+	if(document.getElementById("dice-yes").checked) {
+		gameDice = document.getElementById("dice-yes").value;
+	}
+	else if(document.getElementById("dice-no").checked) {
+		gameDice = document.getElementById("dice-no").value;
+	}
+
+	//Not sure about style...
+	
 });
