4444c226745dff689c238bd056fb0cae2aacff6d
Forrest1711 <forrest.90h@gmail.com>
2020-12-07 01:41:12 -0800

    index.js edits

--- public/index.js
+++ public/index.js
@@ -1,19 +1,38 @@
 let colorButtons = document.querySelector('div.color-option-buttons');
+let confirmOptions = document.querySelector('button.accept-button');
 
-let timerInterval;
-let timing;
-
-
-// used to test functions on click of any button
+// choose game color palette
+let colorChoice;
 colorButtons.addEventListener('click', function (event) {
     let target = event.target;
+    let buttons = event.currentTarget.childNodes;
 
-    // console.log(rollDie(20));
-
-    if (timing === true) {
-        clearInterval(timerInterval);
-        timing = false;
+    for (let i = 3; i < buttons.length; i += 2) {
+        if (buttons[i].style.border = '5px solid white')
+            buttons[i].style.border = '2px solid black';
     }
-    else
-        timer(1);
+    target.style.border = '5px solid white';
+    colorChoice = target.textContent;
+});
+
+let players = document.
+confirmOptions.addEventListener('click', function (event) {
+    // idk, just thinking about how to deal with color choice
+    switch (colorChoice) {
+        case green:
+            break;
+        case blue:
+            break;
+        case yellow:
+            break;
+        case black:
+            break;
+        case white:
+            break;
+        case purple:
+            break;
+
+        default:
+            break;
+    }
 });
