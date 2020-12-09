1ce98e684fb6c3400620100513dde72e99c87559
JPAlmejo <72107371+JPAlmejo@users.noreply.github.com>
2020-12-08 16:22:25 -0800

    Update dice.js

--- public/dice.js
+++ public/dice.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['dice'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
+    return "<div class=\"square\"></div>\r\n<img class=bennyPicture src=\"https://web.engr.oregonstate.edu/~perezalj/Benny/Beaver\" alt=\"Benny\">\r\n<div>\r\n	<button class=\"roll-button\">ROLL!</button>\r\n</div>\r\n<div class=\"roll-results\"></div>\r\n<div class=timeBox></div>\r\n\r\n";
 },"useData":true});
 })();
